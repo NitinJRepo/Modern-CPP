3356ae841d97a5b6096866e81c84b7b4e0ed22c7
NitinJRepo <nitin.jatpuriya@gmail.com>
2019-09-18 20:41:43 +0530

    Added lambda example

---
+++ lambda_function/02-example-with-lambda.cpp
@@ -0,0 +1,14 @@
+// Program to print array elements using lambda function
+#include <iostream>
+#include <algorithm>
+ 
+int main() {
+    int arr[] = { 1, 2, 3, 4, 5 };
+ 
+    std::for_each(arr, arr + sizeof(arr) / sizeof(int), [](int x) {
+            std::cout<<x<<" ";
+        });
+ 
+    std::cout << std::endl;
+ 
+}
